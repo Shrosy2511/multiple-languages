6cb59f449e5e5c90e8e6575c44ba3dec81a02ff8
Shrosy2511 <99042114@mydavinci.nl>
2022-02-17 09:58:09 +0100

    inschakelen uitschakelen kleurtjes

--- lightswitch (1).py
+++ lightswitch (1).py
@@ -1,13 +1,26 @@
 import tkinter as tk
+import random
 window = tk.Tk()
 
 button = tk.Button(text='klik hier!', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+button.pack(pady = 200, padx = 200)
+lst = ['red', 'green', 'purple', 'yellow']
 
 # schijf hier tussen je code
-
-
-
+buttonClicked = 0
+def functie():
+    global buttonClicked
+    if buttonClicked == 0:
+        print('test1')
+        window.config(bg=random.choice(lst))
+        button = tk.Button(text='Hello World', bg="black", fg="white")
+        buttonClicked += 1
+    elif buttonClicked == 1:
+        print('test2')
+        window.config(bg=random.choice(lst))
+        button = tk.Button(text='Hello World', bg="white", fg="black")
+        buttonClicked -= 1
+button.config(command=functie)
 # schijf hier tussen je code
 
 window.mainloop()
